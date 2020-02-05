03aaa915d2e87c1df0c80f196e5794ab0573963f
Ovy95 <jackoverton21@hotmail.com>
2020-02-05 17:09:52 +0000

    Test helpers

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Dave vs. Mittens'
+    expect(page).to have_content 'Mittens: 60HP'
   end
 end
